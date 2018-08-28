92fcd1caa12e0ebfb612850eb6ca00453c1b52d2
Som webster <somnathfreak007@gmail.com>
2018-08-28 20:49:42 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -15,28 +15,6 @@ $(document).ready(function(){
 	/*	Menu item highlighting
 	/* ========================================================================= */
 
-	jQuery('#nav').singlePageNav({
-		offset: jQuery('#nav').outerHeight(),
-		filter: ':not(.external)',
-		speed: 1200,
-		currentClass: 'current',
-		easing: 'easeInOutExpo',
-		updateHash: true,
-		beforeStart: function() {
-			console.log('begin scrolling');
-		},
-		onComplete: function() {
-			console.log('done scrolling');
-		}
-	});
-	
-    $(window).scroll(function () {
-        if ($(window).scrollTop() > 400) {
-            $("#navigation").css("background-color","#0EB493");
-        } else {
-            $("#navigation").css("background-color","rgba(16, 22, 54, 0.2)");
-        }
-    });
 	
 	/* ========================================================================= */
 	/*	Fix Slider Height
